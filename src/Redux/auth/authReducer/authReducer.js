9985f7ad26f541283d53eba26f369243482857d0
asharali95 <52744991+asharali95@users.noreply.github.com>
2020-12-13 11:23:55 +0500

    Done with redux and thunk Integration

---
+++ src/Redux/auth/authReducer/authReducer.js
@@ -0,0 +1,10 @@
+var initialState = null;
+const authReducer = (state = initialState , action) =>{
+    var{type , payload} = action;
+    switch(type) {
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
