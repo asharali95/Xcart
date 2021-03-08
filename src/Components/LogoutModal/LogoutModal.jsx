20d8f09ed6efc9d6d6c4e2fe6d99cbb95000e680
asharali95 <52744991+asharali95@users.noreply.github.com>
2021-03-09 00:45:15 +0500

    logout modal bug fixed

--- src/Components/LogoutModal/LogoutModal.jsx
+++ src/Components/LogoutModal/LogoutModal.jsx
@@ -6,23 +6,34 @@ import ModalContainer from "./../ModalContainer/ModalContainer";
 import { connect } from "react-redux";
 import Button from "./../Button/Button";
 import Header from "./../Header/Header";
+import { Link } from "react-router-dom";
 
 const LogoutModal = ({ signOut, closeModal }) => {
   return (
     <ModalContainer>
       <div className="logout-modal flex" style={{ flexFlow: "column" }}>
         <Header>Are you sure you want to log out?</Header>
+
         <div className="btns">
+          <Link to={"/authentication"}>
+            <Button
+              background="rgb(217, 83, 79)"
+              color="white"
+              onClick={() => {
+                signOut();
+                closeModal();
+              }}
+            >
+              Yes
+            </Button>
+          </Link>
           <Button
-            to="/authentication"
-            onClick={() => {
-              signOut();
-              closeModal();
-            }}
+            background="rgb(131, 131, 243)"
+            color="white"
+            onClick={() => closeModal()}
           >
-            Yes
+            No
           </Button>
-          <Button onClick={() => closeModal()}>No</Button>
         </div>
       </div>
     </ModalContainer>
